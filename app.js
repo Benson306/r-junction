ee4eb702595ea6c53b1de99845cf2bc34e75f8d7
Benson306 <bnkimtai@gmail.com>
2024-08-06 13:09:06 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let app = express();
+
+require('dotenv').config();
+app.use(express.json());
+
+let cors = require('cors');
+app.use(cors());
+
+let mongoose = require('mongoose');
+let mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI);
+
+const path = require('path');
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+
+let port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
+})
